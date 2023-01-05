9c9082b2d353314e02a36d8d5f8859adc35df8ad
EvgenKlo <klokovevgen@gmail.com>
2023-01-05 23:28:15 +0500

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
+
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  let count = [];
+  if (matrix === undefined){
+    return count;
+  };
+  for (let i = 0; i < matrix.length; i++){
+    if (i === 0 || i % 2 === 0){
+      count = count.concat(matrix[i]);
+    } else {
+      count = count.concat(matrix[i].reverse());
+    };    
+  };
+  return count;
+};
